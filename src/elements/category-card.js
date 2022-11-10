1e6cae9e444a51f6fe527204778e8a7fdee1d5f4
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-11-10 16:30:35 +0700

    feat: :sparkles: add list of categories with UI and get fetch from themealdb API

---
+++ src/elements/category-card.js
@@ -0,0 +1,76 @@
+import '@/styles/categoryCard.scss'
+
+class CategoryCard extends HTMLElement {
+  // first, observe the attributes
+  static get observedAttributes() {
+    return ['selected', 'category']
+  }
+
+  constructor() {
+    // Always call super first in constructor
+    super()
+  }
+
+  // lifecycle web components
+  connectedCallback() {
+    this.addEventListener('click', () =>
+      this.setAttribute('selected', this.selected === 'true' ? 'false' : 'true')
+    )
+
+    let data = null
+    if (!this.hasAttribute('selected')) this.setAttribute('selected', null)
+    if (this.hasAttribute('category')) data = JSON.parse(this.category)
+    if (!this.hasAttribute('selected')) this.setAttribute('selected', null)
+
+    if (!data) {
+      this.innerHTML = ``
+    } else if (data) {
+      this.innerHTML = `<div
+              class="tw-cursor-pointer tw-rounded-lg tw-shadow-lg tw-shadow-violet-700 tw-border tw-max-w-[170px] tw-max-h-[188px] tw-bg-gray-800 tw-border-gray-700">
+              <div class="tw-bg-white tw-p-2">
+                  <img class="tw-w-[166px] tw-h-[128px] tw-m-auto tw-rounded-t-lg tw-object-contain"
+                    src="${data.strCategoryThumb}" alt="Category 1" />
+              </div>
+              <div class="tw-p-2">
+                  <h5 class="tw-text-sm tw-font-medium tw-tracking-tight tw-text-white tw-text-center">${data.strCategory}</h5>
+              </div>
+            </div>`
+    }
+  }
+  disconnectedCallback() {}
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    if (this.selected) {
+      const categoryCards = document.querySelectorAll('.category-selected')
+      for (let i = 0; i < categoryCards.length; ++i) {
+        categoryCards[i].classList.remove('category-selected')
+        categoryCards[i].setAttribute('selected', 'false')
+      }
+
+      if (this.selected === 'true') this.classList.add('category-selected')
+    }
+  }
+
+  /**
+   * @param {String} selected
+   */
+  set selected(selected) {
+    this.setAttribute('selected', selected)
+  }
+
+  get selected() {
+    return this.getAttribute('selected')
+  }
+
+  /**
+   * @param {String} category
+   */
+  set category(category) {
+    this.setAttribute('category', category)
+  }
+
+  get category() {
+    return this.getAttribute('category')
+  }
+}
+
+window.customElements.define('category-card', CategoryCard)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,78 @@
+import 'regenerator-runtime/runtime'
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle'
+
+// import styles bundle
+import 'swiper/css/bundle'
+
 // Test import of styles
 import '@/styles/index.scss'
 
-const navbar = document.createElement('nav')
-const navLogo = document.createElement('span')
-navbar.appendChild(navLogo)
+// import web components
+import '@/elements/category-card'
 
-const app = document.querySelector('#root')
-app.append()
+// fetch library/framework
+import axios from 'axios'
+
+// init Swiper:
+const swiper = new Swiper('.categoriesSwiper', {
+  slidesPerView: 7,
+  spaceBetween: 16,
+  breakpoints: {
+    250: {
+      slidesPerView: 1,
+    },
+    350: {
+      slidesPerView: 2,
+    },
+    540: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 5,
+    },
+    1100: {
+      slidesPerView: 6,
+    },
+    1280: {
+      slidesPerView: 7,
+    },
+  },
+  freeMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+async function getListCategories() {
+  try {
+    const categories = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+    if (categories.data && categories.data.categories) return categories.data.categories
+    return null
+  } catch (error) {
+    console.error('got error from API', error)
+    alert('Something error, please reload')
+  }
+}
+
+const swiperWrapper = document.querySelector('.categoriesSwiper .swiper-wrapper')
+getListCategories()
+  .then((listCategories) => {
+    if (!listCategories) swiperWrapper.textContent = 'No data found'
+
+    if (listCategories) {
+      for (let idxCatCard = 0; idxCatCard < listCategories.length; idxCatCard++) {
+        const listCategory = listCategories[idxCatCard]
+        const categoryCard = document.createElement('category-card')
+        categoryCard.className = 'swiper-slide'
+        categoryCard.setAttribute('selected', false)
+        categoryCard.setAttribute('category', JSON.stringify(listCategory))
+        swiperWrapper.appendChild(categoryCard)
+      }
+    }
+  })
+  .catch(() => {
+    swiperWrapper.textContent = 'Error...'
+  })
